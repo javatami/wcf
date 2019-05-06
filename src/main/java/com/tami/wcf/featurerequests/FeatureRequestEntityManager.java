f80680890fa9702e59eb8856a4e89ebbf22bfeb3
Tami Mandarino <tporter@myriad.com>
2019-05-06 07:30:53 -0600

    Tweaks to facilitate spring boot up

---
+++ src/main/java/com/tami/wcf/featurerequests/FeatureRequestEntityManager.java
@@ -0,0 +1,9 @@
+package com.tami.wcf.featurerequests;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+@Configuration
+@EnableJpaRepositories
+public class FeatureRequestEntityManager {
+}
