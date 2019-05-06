fa77e56d9b15db86008e087de12f990d43636116
Tami Mandarino <tporter@myriad.com>
2019-05-06 12:16:15 -0600

    Writing some initialization tests

--- src/main/java/com/tami/wcf/featurerequests/repository/LoadData.java
+++ src/main/java/com/tami/wcf/featurerequests/repository/LoadData.java
@@ -7,21 +7,27 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.util.ArrayList;
+
 @Configuration
 @Slf4j
 public class LoadData {
 
     @Bean
-    CommandLineRunner initClients(ClientRepository clientRepository,
+    CommandLineRunner initRepositories(ClientRepository clientRepository,
                                   ProductAreaRepository productAreaRepository) {
         return args -> {
-           log.info("Loading " + clientRepository.save(new Client("Client A")));
-           log.info("Loading " + clientRepository.save(new Client("Client B")));
-           log.info("Loading " + clientRepository.save(new Client("Client C")));
-           log.info("Loading " + productAreaRepository.save(new ProductArea("Policies")));
-           log.info("Loading " + productAreaRepository.save(new ProductArea("Billing")));
-           log.info("Loading " + productAreaRepository.save(new ProductArea("Claims")));
-           log.info("Loading " + productAreaRepository.save(new ProductArea("Reports")));
+            ArrayList<Client> clients = new ArrayList<>();
+            clients.add(clientRepository.save(new Client("Client A")));
+            clients.add(clientRepository.save(new Client("Client A")));
+            clients.add(clientRepository.save(new Client("Client A")));
+            log.info("Loaded clients:" + clients.toString());
+            ArrayList<ProductArea> productAreas = new ArrayList<>();
+            productAreas.add(productAreaRepository.save(new ProductArea("Policies")));
+            productAreas.add(productAreaRepository.save(new ProductArea("Billing")));
+            productAreas.add(productAreaRepository.save(new ProductArea("Claims")));
+            productAreas.add(productAreaRepository.save(new ProductArea("Reports")));
+            log.info("Loaded productAreas:" + productAreas.toString());
         };
     }
 }
--- src/test/java/com/tami/wcf/featurerequests/FeatureRequestsApplicationTests.groovy
+++ src/test/java/com/tami/wcf/featurerequests/FeatureRequestsApplicationTests.groovy
@@ -1,24 +1,22 @@
-package com.tami.wcf.featurerequests;
+package com.tami.wcf.featurerequests
 
-import com.tami.wcf.featurerequests.controller.FeatureRequestController;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
+import com.tami.wcf.featurerequests.controller.FeatureRequestController
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.test.context.junit4.SpringRunner
-import spock.lang.Specification;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class FeatureRequestsApplicationTests extends Specification {
+public class FeatureRequestsApplicationTests {
 
 	@Autowired
 	FeatureRequestController featureRequestController
 
 	@Test
-	public void contextLoads() {
-		expect: "The feature request controller to have loaded its context"
-		featureRequestController != null
+	void contextLoads() {
+		assert(featureRequestController != null)
 	}
 
 }
